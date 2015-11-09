55e55a8a68a4fa9d0fc004c9da1e4985c6968ec4
Emilio Isaias-Camacho <ga99jypo@stud.informatik.uni-erlangen.de>
2015-11-09 13:45:27 +0100

    Few modifications

--- src/digitalBreastTomosynthesis/BreastPhantom.java
+++ src/digitalBreastTomosynthesis/BreastPhantom.java
@@ -22,14 +22,17 @@ public class BreastPhantom extends Grid3D{
 	public BreastPhantom(int width, int height, int depth) {		
 		super(width, height, depth);
 		if ((depth/width) <= 1){
-			for (int i = 0;i <= width;i++){
-				float x = i - (width/2);
-				for (int j = 0;j <= height;j++){
+			for (int i = 0;i < width;i++){
+				float x = -i;// - (width/2);
+				double x_2 = Math.pow(x, 2);
+				for (int j = 0;j < height;j++){
 					float y = j - (height/2);
-					for (int k = 0;k <= depth;k++){
+					double y_2 = Math.pow(y, 2);
+					for (int k = 0;k < depth;k++){
 						float z = k - (depth/2);
-						double gauss = (1/2*Math.PI*1) * Math.exp(-(x + y + z));
-						
+						double z_2 = Math.pow(z, 2);
+						double gauss = (1.0/2.0*Math.PI*1.0) * Math.exp(-(x_2 + y_2 + z_2)/2.0);
+						super.addAtIndex(i, j, k, (float) gauss);
 					}
 				}
 			}
@@ -37,5 +40,13 @@ public class BreastPhantom extends Grid3D{
 		
 		// TODO Auto-generated constructor stub
 	}
-
+	public static void main(String [] args){
+		int width,height,depth;
+		width = 200;
+		height = 200;
+		depth = 100;
+		BreastPhantom bp = new BreastPhantom(width,height,depth);
+		bp.show();
+		
+	}
 }
